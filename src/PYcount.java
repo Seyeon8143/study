fc9c27022bcf6a06caa2e0902d35b55023c99228
seyeon <sykim@go.byuh.edu>
2020-07-15 03:15:30 +0900

    가운데 글자 가져오기 푼것

--- src/PYcount.java
+++ src/PYcount.java
@@ -1,23 +1,22 @@
 public class PYcount {
-    boolean solution(String s) {
-        //p와 y 의 개수
-        int p = 0;
-        int y = 0;
-        //문자열 s 의 elment 를 가져와서 s문자열의 i 번째 캐릭터가 p이거나 P일때 카운트
-        for(int i =0; i< s.length(); i++){
-            if(s.charAt(i)=='p' || s.charAt(i)=='P'){
-                p++;
+        boolean solution (String s){
+            //p와 y 의 개수
+            int p = 0;
+            int y = 0;
+            //문자열 s 의 elment 를 가져와서 s문자열의 i 번째 캐릭터가 p이거나 P일때 카운트
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+                    p++;
+                }
+                if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+                    y++;
+                }
             }
-            if(s.charAt(i)=='y' || s.charAt(i)=='Y'){
-                y++;
+            //p와 y의 개수가 같으면 true 다르면 false
+            if (p == y) {
+                return true;
+            } else {
+                return false;
             }
-        }
-        //p와 y의 개수가 같으면 true 다르면 false
-        if(p==y){
-            return true;
-        }
-        else{
-            return false;
         }
     }
-}
--- src/PickMiddle.java
+++ src/PickMiddle.java
@@ -1,2 +1,16 @@
 public class PickMiddle {
+    public String solution(String s) {
+        //글자의 길이
+        int len = s.length();
+        //길이가 홀수일때
+        if(len % 2 !=0){
+            return String.valueOf(s.charAt(len/2));
+        }
+        //짝수일때
+        else{
+            return String.valueOf(s.charAt(len/2-1);//String.valueOf(s.charAt(len/2));
+
+        }
+    }
+
 }
