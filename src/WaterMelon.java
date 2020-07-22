38b15d68ae023bc085702128c3ac3d9cffa3f33e
seyeon <sykim@go.byuh.edu>
2020-07-23 02:10:39 +0900

    수박수박수문제풀이1

--- src/WaterMelon.java
+++ src/WaterMelon.java
@@ -1,2 +1,21 @@
 public class WaterMelon {
+    public String waterMelon(int n) {
+    String answer = "";
+    for(int i=0; i<n; i++){
+        //짝수일때
+        if(i%2==0){
+            answer = answer +"수";
+        }
+        if(i%2!=0){
+            answer = answer +"박";
+        }
+    }
+    return answer;
+}
+    public static void main(String[] args){
+        WaterMelon wm = new WaterMelon();
+        System.out.println("n이 3인 경우: " + wm.waterMelon(3));
+        System.out.println("n이 4인 경우: " + wm.waterMelon(4));
+
+    }
 }
